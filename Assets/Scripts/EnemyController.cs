515376fefc12a88b2dbbcae2b7babc5de91b46d7
ANSON FENG <u1529790@ad.utah.edu>
2024-08-26 17:12:21 -0600

    Add enemy logic template

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector2 direction;
+
+    [Header("Enemy Attribute")]
+    public float Speed = 1.0f;
+    public float ShootCoolDown = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
