62fc0723fed730729bf816bcb9adb3cdb7bcbe26
Taihou <ansonfeng1999@gmail.com>
2024-08-27 11:07:10 -0600

    Enemy logic half implemented

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Vector2 direction;
+    private Rigidbody2D rb;
 
     [Header("Enemy Attribute")]
     public float Speed = 1.0f;
     public float ShootCoolDown = 2.0f;
+    public Centipede centipede;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    private void Awake() 
+    { 
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 targetPos = GridPosition(targetHead.transform.position);
+        Vector2 currPos = transform.position;
+        float currSpeed = Speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(currPos, targetPos, currSpeed);
+    }
+
+    /// <summary>
+    /// Borrow from CentipedeSection, getting the position in the grid.
+    /// </summary>
+    /// <param name="position">Current position in 2D.</param>
+    /// <returns>Accurate Integer position</returns>
+    private Vector2 GridPosition(Vector2 position)
+    {
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+        return position;
     }
 }
